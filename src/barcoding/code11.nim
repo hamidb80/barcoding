9a50c8c372aece2370318230a00a5d3270973969
hamidb80 <hr.bolouri@gmail.com>
2022-11-12 19:53:22 +0330

    init code 11

--- src/barcoding/code11.nim
+++ src/barcoding/code11.nim
@@ -1,9 +1,11 @@
+## https://en.wikipedia.org/wiki/Code_11
+
 import common
 
 type
   Code11 = enum
     c0, c1, c2, c3, c4, c5, c6, c7, c8, c9
-    cDash, cStart
+    cDash, cStartStop
 
 func toCode11(ch: char): Code11 =
   case ch
@@ -20,5 +22,24 @@ func toCode11(ch: char): Code11 =
   of '-': cDash
   else: raise newException(ValueError, "not valid")
 
+func bits(c: Code11): seq[bool] =
+  case ch
+  of c0: @[]
+  of c1: @[]
+  of c2: @[]
+  of c3: @[]
+  of c4: @[]
+  of c5: @[]
+  of c6: @[]
+  of c7: @[]
+  of c8: @[]
+  of c9: @[]
+  of cDash: @[]
+  of cStartStop: @[]
+
+
 func code11Repr*(data: seq[Code11]): Barcode =
-  discard
+  result.add cStartStop
+
+
+  result.add cStartStop
